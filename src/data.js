16639b75f032676b15cfeb0dcca7194c9059c302
Orlando <lalomax365@gmail.com>
2024-08-07 19:21:15 +0000

    refactor: image adresses

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export default [
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
     description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    imageUrl: "https://imgs.search.brave.com/BiwAorJJccczea5a-TM5krVLPqnD0Y-ufT8zthbN9-M/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9mdWppLW1vdW50/YWluLWthd2FndWNo/aWtvLWxha2UtamFw/YW5fNDAxNzEtMjMz/LmpwZz9zaXplPTYy/NiZleHQ9anBn"
   },
   {
     title: "Sydney Opera House", 
@@ -15,7 +15,7 @@ export default [
     startDate: "27 may, 2021",
     endDate: "8 Jun, 2021",
     description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
-    imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+    imageUrl: "https://imgs.search.brave.com/W_aLQ277pMA6uZXmJlpAGfd2c70tWkInWXLZPxllido/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuYWRzdHRjLmNv/bS9tZWRpYS9pbWFn/ZXMvNTAzNy9lMmZl/LzI4YmEvMGQ1OS85/YjAwLzAyNDEvbmV3/c2xldHRlci9zdHJp/bmdpby5qcGc_MTQx/NDIxODc3OQ"
   },
   {
     title: "Geirangerfjord",
@@ -24,7 +24,7 @@ export default [
     startDate: "1 Oct, 2021",
     endDate: "18 Nov, 2021",
     description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+    imageUrl: "https://imgs.search.brave.com/OYKzvF7csmJ_ELey-ZQsdrh2bip3x_myLwrn1Y3WF1M/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/c2FuaXR5LmlvL2lt/YWdlcy80YWFuczBp/bi9wcm9kdWN0aW9u/LzNmMzgwYzNjOTgz/Yzc1YjE1YzBmOGQx/NjM5MzhjMDE1MmI3/NzgyMDctMjAwMHgy/NzI0LmpwZz9yZWN0/PTAsODAwLDIwMDAs/MTEyNSZ3PTE5MjAm/aD0xMDgwJmZtPXdl/YnAmcT0xMDAmZml0/PW1heCZhdXRvPWZv/cm1hdA"
   }
 
 ]
